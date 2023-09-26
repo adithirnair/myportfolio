60fa65e334a5488dde6adf9575a61e15c705d8d7
adithirnair <139519572+adithirnair@users.noreply.github.com>
2023-09-26 15:53:56 +1000

    Add files via upload

---
+++ src/components/MyProjects.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="projects">
+      <link rel="stylesheet" href="./app.css"> 
+      <h2 style="color:rgb(240, 136, 192)">Projects</h2>
+      <ul>
+        <li v-for="project in projects" :key="project.id">
+          <h3 style="font-size: larger;color: deeppink;">{{ project.title }}</h3>
+          <p>{{ project.description }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        projects: [
+          { id: 1, title: 'ADIS PIZZERIA', description: 'A website for a pizza shop .' },
+          { id: 2, title: 'ARN Beauty', description: 'A website for a beauty brand' },
+          // Add more projects here
+        ],
+      };
+    },
+  };
+ 
+  </script>
+  
---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>I am Adithi , an 18 year old Bachelors of IT student . I love creating new websites .</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
